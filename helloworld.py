e4b7516d28ff8f108b1a89eb0e687f8544c87c13
vraj-desai <desaiv@umich.edu>
2019-09-06 14:52:20 -0400

    added helloworld.py

---
+++ helloworld.py
@@ -0,0 +1,15 @@
+import random
+
+def main():
+    print('Vraj Desai')
+    num1 = random.randint(0, 100)
+    num2 = random.randint(0, 100)
+    print(num1)
+    print(num2)
+    sum = num1+num2
+    print(sum)
+    print(sum/2)
+    
+
+if __name__=='__main__':
+    main()
